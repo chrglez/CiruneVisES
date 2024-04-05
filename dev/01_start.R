ee3ede475474f7285f11794533a138ca4b1b2310
Christian González-Martel <christian.gonzalez@ulpgc.es>
2024-04-05 13:50:33 +0100

    Add README.Rmd and CODE_OF_CONDUCT.md

--- dev/01_start.R
+++ dev/01_start.R
@@ -38,11 +38,11 @@ golem::install_dev_deps()
 ## Create Common Files ----
 ## See ?usethis for more information
 usethis::use_mit_license("Christian Gonzalez") # You can set another license here
-usethis::use_readme_rmd(open = FALSE)
+usethis::use_readme_rmd(open = TRUE)
 devtools::build_readme()
 # Note that `contact` is required since usethis version 2.1.5
 # If your {usethis} version is older, you can remove that param
-usethis::use_code_of_conduct(contact = "Golem User")
+usethis::use_code_of_conduct(contact = "Christian Gonzalez <")
 usethis::use_lifecycle_badge("Experimental")
 usethis::use_news_md(open = FALSE)
 
